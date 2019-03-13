3d2c1d7c4c7d4ece19c6dcb02800a9cd05eeba37
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-03-13 20:51:09 +0800

    layout practice

---
+++ LayoutPractice/DiagonalPanel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayoutPractice
+{
+  public class DiagonalPanel : Panel
+  {
+    protected override Size MeasureOverride(Size availableSize)
+    {
+      var mySize = new Size();
+      foreach (UIElement child in this.InternalChildren)
+      {
+        child.Measure(availableSize);
+        mySize.Width += child.DesiredSize.Width;
+        mySize.Height += child.DesiredSize.Height;
+      }
+
+      return mySize;
+    }
+    protected override Size ArrangeOverride(Size finalSize)
+    {
+      var location = new Point();
+      foreach (UIElement child in this.InternalChildren)
+      {
+        child.Arrange(new Rect(location, child.DesiredSize));
+        location.X += child.DesiredSize.Width;
+        location.Y += child.DesiredSize.Height;
+      }
+
+      return finalSize;
+    }
+  }
+}
